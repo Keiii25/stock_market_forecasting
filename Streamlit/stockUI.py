ac1f6229b3c3ce871b9d09264f57260259871e90
shoo0035 <42R2zjBm3rxmNycsMTMB>
2022-07-28 17:10:21 +0800

    Upload HTML UI file

---
+++ Streamlit/stockUI.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write(""" 
+# Predict Stock with *N-BEATS*
+We are using a meta-learning model called N-BEATS to help traders to forecast the future price of an individual publicly traded stock.
+      
+""")
