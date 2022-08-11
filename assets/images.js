021d71a7974628b133f4b00303f2e5ab142ae10e
Kar Kei Ong <candyokk@outlook.com>
2022-08-12 03:33:47 +0800

    Generate image variable for easy imports

---
+++ assets/images.js
@@ -0,0 +1,15 @@
+//Run command ' node scripts/generate-images.js ' at root folder to auto regenerate this file when adding / removing images from app/assets/image folder  
+  
+ const images = {
+  add_icon: require('./images/add_icon.png'),
+  apple: require('./images/apple.png'),
+  arrow_down: require('./images/arrow_down.png'),
+  calendar: require('./images/calendar.png'),
+  chart: require('./images/chart.png'),
+  logo_white: require('./images/logo_white.png'),
+  search_dollar: require('./images/search_dollar.png'),
+  search_icon: require('./images/search_icon.png'),
+  triangle: require('./images/triangle.png')
+}
+
+export default images;
